a2e0390e69fb4164f9dff86493b315ed1e0a7619
Nourah Alqhtani <nourah0qq@gmail.com>
2022-11-14 04:44:43 +0300

    add register and login

--- IntentWebsite/Intent/urls.py
+++ IntentWebsite/Intent/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = "Intent"
 
 urlpatterns = [
-    path("home/", views.home, name="home")
+    path("front/", views.front, name="front"),
+    path("register/", views.register , name ="register"),
+    path("signin/", views.signin , name ="signin"),
+    path("home/", views.home, name="home"),
+    path("profile/",views.profile,name = "profile"),
+    path("search/",views.profile,name = "profile"),
 ]
--- IntentWebsite/Intent/views.py
+++ IntentWebsite/Intent/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def front(request:HttpResponse):
+    return render(request,"Intent/front.html")
+
+def register(request:HttpResponse):
+    return render(request,"Intent/register.html")
+
+def signin(request:HttpResponse):
+    return render(request,"Intent/signin.html")
+
 def home(request:HttpResponse):
     return render(request,"Intent/home.html")
+
+def profile(request:HttpResponse):
+    return render(request,"Intent/profile.html")
+
+def search(request:HttpResponse):
+    return render(request,"Intent/search.html")
